f5f49dd0bbd58c412c460818388feaf86545c2d8
Javier Uruen Val <juruen@spotify.com>
2013-07-29 11:12:41 +0200

    Fix random_var_function

--- probability.py
+++ probability.py
@@ -4,26 +4,24 @@ import sets
 
 BOARD_SIZE = 9
 MINES = 10
-SAMPLES = 10**5
+SAMPLES = 10**6
 
 def generate_mines(board_size, num_of_mines):
-    mines = sets.Set()
-    while len(mines) != num_of_mines:
-        mines.add((random.randint(0, board_size - 1),
-                  random.randint(0, board_size - 1)))
-    return mines
+    return [(i / BOARD_SIZE, i % BOARD_SIZE) for i in random.sample(range(0, BOARD_SIZE**2), MINES)]
 
-def has_adjacent(pos, mines):
+def adjacents(pos, mines):
     x, y = pos
-    return sets.Set([(x + 1, y), (x -1, y), (x, y + 1), (x, y -1),
+    return sets.Set([(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1),
                        (x + 1, y + 1), (x + 1 ,y - 1),
                        (x - 1, y + 1), (x - 1, y - 1)]).intersection(mines)
 
 def random_var_function(mines):
-    for mine in mines:
-        if not has_adjacent(mine, mines):
-            return 0
-    return 1
+    can_reach = adjacents(mines.pop(), mines)
+    while can_reach:
+        pos = can_reach.pop()
+        if pos in mines:
+            mines.remove(pos)
+            can_reach = can_reach.union(adjacents(pos, mines))
+    return int(not bool(mines))
 
 print sum([random_var_function(generate_mines(BOARD_SIZE, MINES)) for i in range(0, SAMPLES)]) / float(SAMPLES)
-
